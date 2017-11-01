c8268bd2dd3445e306ac14e1a53c71dfdcb39a82
dadgoincrazy <dennhardtc12@gmail.com>
2017-10-31 19:41:16 -0700

    Initial Commit

---
+++ app/src/main/java/com/trucs/comp2160_project/MapListActivity.java
@@ -0,0 +1,13 @@
+package com.trucs.comp2160_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_list);
+    }
+}
