2252edaae160304c924f70ccd9d599eccd67b95c
dadgoincrazy <dennhardtc12@gmail.com>
2017-11-01 10:44:49 -0700

    Update

---
+++ app/src/main/java/com/trucs/comp2160_project/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.trucs.comp2160_project;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    final static int TIME = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent myIntent = new Intent(SplashActivity.this, MapListActivity.class);
+                startActivity(myIntent);
+                finish();
+            }
+        }, TIME);
+    }
+}
