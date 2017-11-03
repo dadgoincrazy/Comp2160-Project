6c2d050ea730a522a87e12b72fa2ace0a4ea58a2
dadgoincrazy <31976620+dadgoincrazy@users.noreply.github.com>
2017-11-02 19:01:51 -0700

    Update

--- app/src/main/java/com/trucs/comp2160_project/MapListActivity.java
+++ app/src/main/java/com/trucs/comp2160_project/MapListActivity.java
@@ -1,9 +1,14 @@
 package com.trucs.comp2160_project;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
-public class MapListActivity extends AppCompatActivity {
+public class MapListActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
